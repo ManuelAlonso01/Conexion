da66e6b1c148c0a9c5ed5d1e422f7ff7e7c03835
Manuel Alonso <153567509+ManuelAlonso01@users.noreply.github.com>
2025-11-29 11:41:34 -0300

    Refactor server.py to remove authentication
    Removed authentication logic and simplified command execution.

--- server.py
+++ server.py
@@ -1,63 +1,31 @@
 import socket
 import subprocess
-import bcrypt
-import json
-
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Ejemplo: server_socket.bind(("111.111.111.111", 5000))
-server_socket.bind(("TU IP", "ELEGIR PUERTO")) # Puerto recomendado: 5000
-
+server_socket.bind(("192.168.1.56", 5000))
 server_socket.listen(1)
 print("Esperando conexión...")
 
-
-client_socket, client_address = server_socket.accept() 
+client_socket, client_address = server_socket.accept()
 print(f"Cliente conectado desde {client_address}")     
+                                                   
 
 
-with open("usuario.json", "r") as file:
-    data = json.load(file)
+while True:
 
-intentos = 3
-autenticado = False
+    comando = client_socket.recv(1024).decode()
+    if comando.lower() == "exit":
+        break
 
-while intentos >= 0 and not autenticado:
-    # Recivimos la contraseña
-    password = client_socket.recv(1024).decode()  
-    # Verificamos si es la que tenemos almacenada
-    if bcrypt.checkpw(password.encode(), data["hash"].encode()):
-        client_socket.send("Acceso concedido".encode())
-        autenticado = True
-    else:
-        client_socket.send(f"Contraseña incorrecta le quedan {intentos} intentos".encode())
-        intentos -= 1
-        
-        
-if autenticado:
-    while True:
-        comando = client_socket.recv(1024).decode()  # recibir comando
-        if comando.lower() == "exit":
-            client_socket.close()
-            break
-        resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
-        salida = "Server: " + resultado.stdout + resultado.stderr
-        client_socket.send(salida.encode())  # enviar resultado de vuelta
-else:
-    client_socket.send(b"ERROR: Demasiados intentos. Conexion cerrada.") 
-# Cerrar conexión
+    resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    salida = resultado.stdout + resultado.stderr
+    
+    client_socket.send(salida.encode())
+
 client_socket.close()
 server_socket.close()
-
-
-
-
-
-
-
-
-
 
 
 
